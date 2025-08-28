e921fedf85628ee62f8e47878ff91ee99319bbce
Demi Dev Unit <131922119+jackandcarter@users.noreply.github.com>
2025-08-27 23:10:17 -0500

    Delete DemiCatPlugin directory

--- DemiCatPlugin/ChannelDto.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace DemiCatPlugin;
-
-public class ChannelDto
-{
-    [JsonPropertyName("id")] public string Id { get; set; } = string.Empty;
-    [JsonPropertyName("name")] public string Name { get; set; } = string.Empty;
-}
