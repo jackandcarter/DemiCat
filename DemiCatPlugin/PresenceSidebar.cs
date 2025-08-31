3babe2e159619ee4c01185e1968d8149e9f2a939
Demi Dev Unit <131922119+jackandcarter@users.noreply.github.com>
2025-08-30 22:25:30 -0500

    Refresh presence list on WebSocket reconnection

--- DemiCatPlugin/PresenceSidebar.cs
+++ DemiCatPlugin/PresenceSidebar.cs
@@ -147,6 +147,8 @@ public class PresenceSidebar : IDisposable
                 }
                 var uri = BuildWebSocketUri();
                 await _ws.ConnectAsync(uri, token);
+                _loaded = false;
+                _ = Refresh();
                 _ = PluginServices.Instance!.Framework.RunOnTick(() => _statusMessage = string.Empty);
                 var buffer = new byte[1024];
                 while (_ws.State == WebSocketState.Open && !token.IsCancellationRequested)
