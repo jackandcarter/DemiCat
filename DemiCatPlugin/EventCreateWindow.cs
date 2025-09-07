36f8401928b8d286b839ccdf644617cb68a7c122
Demi Dev Unit <131922119+jackandcarter@users.noreply.github.com>
2025-09-07 06:29:29 -0500

    Add Json Serialization using directive

--- DemiCatPlugin/EventCreateWindow.cs
+++ DemiCatPlugin/EventCreateWindow.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Numerics;
 using System.Threading.Tasks;
 using System.Collections.Generic;
