1e7981f811f93f40fbe4c01682b6db45996847fc
Demi Dev Unit <131922119+jackandcarter@users.noreply.github.com>
2025-09-14 23:46:19 -0500

    feat: support camelcase aliases

--- demibot/demibot/http/schemas.py
+++ demibot/demibot/http/schemas.py
@@ -8,8 +8,14 @@ from enum import IntEnum
 
 # ---- Embeds ----
 
+
+def to_camel(string: str) -> str:
+    parts = string.split("_")
+    return parts[0] + "".join(word.capitalize() for word in parts[1:])
+
+
 class CamelModel(BaseModel):
-    model_config = ConfigDict(populate_by_name=True)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
 
 class EmbedFieldDto(CamelModel):
