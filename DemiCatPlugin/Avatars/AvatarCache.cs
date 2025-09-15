ed05d213d2c29e74dccde63bfb1c0c638e55ac24
Demi Dev Unit <131922119+jackandcarter@users.noreply.github.com>
2025-09-15 06:39:59 -0500

    fix: import texture provider

--- DemiCatPlugin/Avatars/AvatarCache.cs
+++ DemiCatPlugin/Avatars/AvatarCache.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dalamud.Interface.Textures;
+using Dalamud.Plugin.Services;
 
 namespace DemiCatPlugin.Avatars;
 
